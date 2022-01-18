53e0504a2a2b459a53c2918f3380b12d3eb99301
ramameghana512 <ramadaliparthi23@gmail.com>
2022-01-18 11:45:55 +0530

    committing

---
+++ src/com/flipkart/demo/Demo1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author Rama
+ *
+ */
+public class Demo1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HI");
+	}
+
+}
